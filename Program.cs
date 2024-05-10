babd56d330f8ea2e36ee27d4c1d8f840179ef3bb
smaitland <smaitland@WKS-JHBE4-321>
2024-05-10 08:50:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+         Vehicle obj = new Vehicle();
+        Vehicle obj2 = new Vehicle(2);
+
+       // VehicleTwo objTwo = new VehicleTwo();
+    }
+    
+}
+
+public class Vehicle
+{
+    public Vehicle()
+    {
+        Console.WriteLine("This is a brand new Vehicle");
+    }
+    public Vehicle(int Tyrecount)
+    {
+        Console.WriteLine("Vehicle has two tyres");
+    }
+
+   
+  
+}
+
+//public class VehicleTwo
+//{
+   // public VehicleTwo()
+ //   {
+   //     Console.WriteLine("this is our second car");
+ //   }
+//}
+
